fd1fd77466b2dbab79852c00843dbff0dc26fbcd
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-08-18 21:17:33 +0600

    Хрень...

--- Module8Task3/Program.cs
+++ Module8Task3/Program.cs
@@ -7,7 +7,61 @@ namespace Module8Task3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\YOGA\Desktop\C# SkillFactory\WaitingForDeath");
+            try
+            {
+                if (directory.Exists)
+                {
+                    long size = 0;
+                    long fileCount = 0;
+                    long totalsize = EvaluateSpace(size, directory);
+                    Console.WriteLine($"Исходный размер папки {directory}: {totalsize} байт");
+                    long deletedfiles = DeleteAndCount(fileCount, directory);
+                    Console.WriteLine($"Удалено файлов: {deletedfiles}");
+                    long newtotalsize = EvaluateSpace(size, directory);
+                    Console.WriteLine($"Освобождено: {totalsize - newtotalsize}");
+                    Console.WriteLine($"Текущий размер папки: {newtotalsize}");
+                }
+                else
+                {
+                    throw new Exception("Такой папки не существует");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+
+        static long EvaluateSpace(long size, DirectoryInfo dirSpace)
+        {
+            foreach (FileInfo file in dirSpace.GetFiles())
+            {
+                size += file.Length;
+            }
+            foreach (DirectoryInfo papka in dirSpace.GetDirectories())
+            {
+                size += EvaluateSpace(0, papka);
+            }
+            return size;
+        }
+
+        static long DeleteAndCount(long fileCount, DirectoryInfo dirSpace)
+        {
+            DateTime thirty = DateTime.Now - TimeSpan.FromMinutes(30);
+            foreach (FileInfo file in dirSpace.GetFiles())
+            {
+                if(file.LastAccessTime < thirty)
+                {
+                    fileCount++;
+                    file.Delete();
+                }
+            }
+            foreach(DirectoryInfo papka in dirSpace.GetDirectories())
+            {
+                DeleteAndCount(0, papka);
+            }
+            return fileCount;
         }
     }
 }
