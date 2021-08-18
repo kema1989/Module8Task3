159bd437ac604e46fe7ce0c0df85505232e65a22
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-08-18 20:28:39 +0600

    Добавьте файлы проекта.

---
+++ Module8Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Module8Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
